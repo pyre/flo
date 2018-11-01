1a43cba520fed8c91aa650a84e775214b197378c
Alec Aivazis <alec@aivazis.com>
2018-11-01 00:53:02 -0700

    added prettier and hello world

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// external imports
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const HelloWorld = () => 'hello world'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
