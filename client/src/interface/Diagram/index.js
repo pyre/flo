8502616a1c6996e9a7b3d8a47cb9dc7d91e8bd5e
Alec Aivazis <alec@aivazis.com>
2018-11-16 00:54:14 -0800

    pulled diagram pan and zoom behavior into separate hooks

--- client/src/interface/Diagram/index.js
+++ client/src/interface/Diagram/index.js
@@ -9,49 +9,25 @@ import SelectionRectangle from './SelectionRectangle'
 import * as styles from './styles'
 
 export default () => {
+    // grab the info and actions we need from the diagram
+    const { diagram } = useContext(DiagramContext)
+
     // we need a ref to track if the mouse is clicked on the element
     const elementRef = useRef(null)
 
     // we care if the space bar is pressed
     const spacePressed = useKeyPress(' ')
+
     // track the movement of the mouse
     const mouseDrag = useMouseDrag(elementRef.current, [spacePressed])
 
-    // grab the info and actions we need from the diagram
-    const { pan, zoomIn, zoomOut, diagram } = useContext(DiagramContext)
+    // enable the zoom behavior
+    useZoomBehavior(elementRef)
+    // and the drag behavior
+    useDragBehavior({ spacePressed, mouseDrag })
 
     // wether or not we're panning the diagram
     const panning = spacePressed && mouseDrag
-
-    // the keyboard interactions have all sorts of effects
-    useEffect(
-        () => {
-            // if the space bar is pressed and the mouse is being moved
-            if (spacePressed && mouseDrag) {
-                // pan the diagram
-                pan(mouseDrag.delta)
-            }
-        },
-        [spacePressed, mouseDrag && mouseDrag.delta.x, mouseDrag && mouseDrag.delta.y]
-    )
-
-    // when the wheel scrolls
-    useEvent('mousewheel', event => {
-        // make sure that the scrolling only happens on the diagram
-        if (event.target !== elementRef.current) {
-            return
-        }
-
-        // if the user scrolled up
-        if (event.wheelDelta / 120 > 0) {
-            // zoom in
-            zoomIn()
-            // otherwise we scrolled down
-        } else {
-            // zoom the diagram out
-            zoomOut()
-        }
-    })
 
     // compute the transform string for the diagram
     const { transformString } = SvgMatrix()
@@ -72,4 +48,43 @@ export default () => {
             </g>
         </svg>
     )
+}
+
+const useDragBehavior = ({ mouseDrag, spacePressed }) => {
+    const { pan } = useContext(DiagramContext)
+
+    // the keyboard interactions have all sorts of effects
+    useEffect(
+        () => {
+            // if the space bar is pressed and the mouse is being moved
+            if (spacePressed && mouseDrag) {
+                // pan the diagram
+                pan(mouseDrag.delta)
+            }
+        },
+        [spacePressed, mouseDrag && mouseDrag.delta.x, mouseDrag && mouseDrag.delta.y]
+    )
+}
+
+const useZoomBehavior = elementRef => {
+    // grab the info and actions we need from the diagram
+    const { zoomIn, zoomOut } = useContext(DiagramContext)
+
+    // when the wheel scrolls
+    useEvent('mousewheel', event => {
+        // make sure that the scrolling only happens on the diagram
+        if (event.target !== elementRef.current) {
+            return
+        }
+
+        // if the user scrolled up
+        if (event.wheelDelta / 120 > 0) {
+            // zoom in
+            zoomIn()
+            // otherwise we scrolled down
+        } else {
+            // zoom the diagram out
+            zoomOut()
+        }
+    })
 }
