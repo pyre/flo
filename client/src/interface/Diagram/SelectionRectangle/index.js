ce6a736b3d9d38a2c83d26143b4a52d17513d26b
Alec Aivazis <alec@aivazis.com>
2018-11-16 00:38:10 -0800

    selection rectangle accomodates zoom

--- client/src/interface/Diagram/SelectionRectangle/index.js
+++ client/src/interface/Diagram/SelectionRectangle/index.js
@@ -4,31 +4,32 @@ import { useRelativePosition } from '~/hooks'
 
 export default ({ point1, point2 }) => {
     // compute the relative locations for the points
-    const p1 = useRelativePosition(point1)
+    const relativePoint1 = useRelativePosition(point1)
+    const relativePoint2 = useRelativePosition(point2)
 
     // compute the dimensions of the rectangle
-    const width = Math.abs(point2.x - point1.x)
-    const height = Math.abs(point2.y - point1.y)
+    const width = Math.abs(relativePoint2.x - relativePoint1.x)
+    const height = Math.abs(relativePoint2.y - relativePoint1.y)
 
     // figure out the transform to align the rectangle with the user's mouse
     const transform = [1, 0, 0, 1, 0, 0]
 
     // if the origin is to the right of the mouse
-    if (point1.x > point2.x) {
+    if (relativePoint1.x > relativePoint2.x) {
         // we need to move the rectangle to the left by its width
         transform[4] = -width
     }
 
     // if the origin is above the rectangle
-    if (point1.y > point2.y) {
+    if (relativePoint1.y > relativePoint2.y) {
         // we need to translate the rectangle down by its height
         transform[5] = -height
     }
 
     return (
         <rect
-            x={p1.x}
-            y={p1.y}
+            x={relativePoint1.x}
+            y={relativePoint1.y}
             width={width}
             height={height}
             transform={`matrix(${transform.join(',')})`}
