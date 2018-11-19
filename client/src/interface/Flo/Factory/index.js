523cf79acd417920af1be9ad1c9f216ddab2e986
Alec Aivazis <alec@aivazis.com>
2018-11-19 03:25:29 -0500

    added square anchors to factories

--- client/src/interface/Flo/Factory/index.js
+++ client/src/interface/Flo/Factory/index.js
@@ -5,7 +5,12 @@ import { createFragmentContainer, graphql } from 'react-relay'
 import { elementOutline, factoryPrimary } from '~/colors'
 
 // the primary length for the central daimon
-const diamondLength = 10
+const diamondLength = 15
+// the primary length for the squares that define the in and out point
+const squareLength = 5
+
+// the distance from the central diamond to the square
+const armLength = 50
 
 const Factory = ({
     factory: {
@@ -13,6 +18,15 @@ const Factory = ({
     },
 }) => (
     <>
+        // there is a line going from one square to the other
+        <line
+            stroke={elementOutline}
+            strokeWidth={2}
+            x1={x + armLength}
+            y1={y}
+            y2={y}
+            x2={x - armLength}
+        />
         // the center of a factory is the diamond used as the target for clicks
         <polygon
             fill={factoryPrimary}
@@ -21,6 +35,27 @@ const Factory = ({
             strokeLinejoin="round"
             points={`${x - diamondLength},${y} ${x},${y + diamondLength} ${x +
                 diamondLength},${y} ${x},${y - diamondLength}`}
+        />
+        // the input and output node for a factory is represented by small boxes 50px outerHeight
+        <rect
+            fill={factoryPrimary}
+            stroke={elementOutline}
+            strokeWidth={2}
+            strokeLinejoin="round"
+            x={x - armLength - squareLength}
+            y={y - squareLength}
+            width={2 * squareLength}
+            height={2 * squareLength}
+        />
+        <rect
+            fill={factoryPrimary}
+            stroke={elementOutline}
+            strokeWidth={2}
+            strokeLinejoin="round"
+            x={x + armLength - squareLength}
+            y={y - squareLength}
+            width={2 * squareLength}
+            height={2 * squareLength}
         />
     </>
 )
