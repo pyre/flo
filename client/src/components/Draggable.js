897f17fa39f2fc788e0282d7445c97c312303c34
Alec Aivazis <alec@aivazis.com>
2019-02-03 12:43:09 -0800

    onMove handler is called when appropriate

--- client/src/components/Draggable.js
+++ client/src/components/Draggable.js
@@ -1,10 +1,10 @@
 // externals
-import React, { useRef, useContext, useEffect } from 'react'
+import React, { useRef, useContext, useEffect, useLayoutEffect } from 'react'
 // local imports
-import { useMouseDrag } from '~/hooks'
+import { useMouseDrag, useNormalDelta } from '~/hooks'
 import { DiagramContext } from '~/state'
 
-const Draggable = ({ id, children, onMove, origin }) => {
+const Draggable = ({ id, children, origin, onMove }) => {
     const elementRef = useRef(null)
 
     // track the movement of the mouse
@@ -14,16 +14,37 @@ const Draggable = ({ id, children, onMove, origin }) => {
     // we need to make sure that we are seleted if we are dragging
     useEffect(
         () => {
-            mouseDrag && (console.log('selecting', id) || selectElements(id))
+            mouseDrag && selectElements(id)
         },
         [mouseDrag.origin, id]
     )
 
-    // if we are dragging the mouse and the delta is greater
-    // than half of the grid size after applying zoom
-    if (mouseDrag) {
-        console.log('dragging draggable', id)
-    }
+    // compute the amount we've moved in the x direction
+    const deltaX = useNormalDelta(mouseDrag && mouseDrag.currentLocation.x - origin.x)
+    // compute the amount we've moved in the y direction
+    const deltaY = useNormalDelta(mouseDrag && mouseDrag.currentLocation.y - origin.y)
+
+    // we need to kick off the onMove handler if we have moved beyond a grid cell
+    useLayoutEffect(
+        () => {
+            // if we are dragging the draggable beyond a grid cell
+            if (Math.abs(deltaX) > 0.5 || Math.abs(deltaY) > 0.5) {
+                // we have to figure out the new coordinates for the element
+                const coordinates = origin
+
+                // round the two deltas to the nearest whole number of grid cells
+                const [roundX, roundY] = [deltaX, deltaY].map(Math.round).map(coord => coord * diagram.gridSize)
+
+                // the new coordinates for the object
+                const newX = origin.x + roundX
+                const newY = origin.y + roundY
+
+                // trigger the move handlers
+                onMove && onMove({ x: newX, y: newY })
+            }
+        },
+        [deltaX, deltaY]
+    )
 
     return <g ref={ref => (elementRef.current = ref)}>{children}</g>
 }
---
+++ client/src/hooks/useNormalDelta.js
@@ -0,0 +1,24 @@
+// external imports
+import { useContext } from 'react'
+// local imports
+import { DiagramContext } from '~/state'
+import { round } from '~/utils'
+
+// this hook normalizes a given delta by the grid size. If the grid size is 50, and the delta is 25
+// this hook returns .5 (half of a grid)
+export default delta => {
+    // pull the diagram information
+    const { diagram } = useContext(DiagramContext)
+
+    // if there is no grid size
+    if (diagram.gridSize === 0) {
+        // then we have moved infinite more than the grid size
+        return Infinity
+    }
+
+    // convert the grid size into viewport coordinates
+    const zoomGridSize = diagram.gridSize * diagram.zoomLevel
+
+    // return the amount we've moved normalized by the grid
+    return delta / zoomGridSize
+}
