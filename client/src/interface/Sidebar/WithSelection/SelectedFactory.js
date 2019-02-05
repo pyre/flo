dc2c7c2a791becb14b2e8a0775239075c801f3f4
Alec Aivazis <alec@aivazis.com>
2019-02-04 16:07:18 -0800

    removed unused imports

--- client/src/interface/Sidebar/WithSelection/SelectedFactory.js
+++ client/src/interface/Sidebar/WithSelection/SelectedFactory.js
@@ -1,8 +1,6 @@
 // external imports
 import React from 'react'
 import { createFragmentContainer, graphql } from 'react-relay'
-// local imports
-import { SelectedFactory_Factory } from './__generated__/SelectedFactory_Factory.graphql'
 
 const SelectedFactory = ({ factory }) => <div>selected factory</div>
 
--- client/src/interface/Sidebar/WithSelection/SelectedProduct.js
+++ client/src/interface/Sidebar/WithSelection/SelectedProduct.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import { createFragmentContainer, graphql } from 'react-relay'
 // local imports
-import { SelectedProduct_product } from './__generated__/SelectedProduct_product.graphql'
 
 const SelectedProduct = ({ product }) => <div>selected product</div>
 
