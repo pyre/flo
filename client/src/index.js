8928f5586831eada1f4515790e13d220a5ad2658
Alec Aivazis <alec@aivazis.com>
2019-03-15 11:27:29 -0700

    removed App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 // external imports
-import ReactDOM from 'react-dom'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import '@babel/polyfill'
+import { App as AppContainer, AlertContainer } from 'quark-web'
 // local imports
-import { App } from './interface'
+import { DiagramProvider } from '~/state'
+import { Diagram, Sidebar } from '~/interface'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const App = () => {
+    return (
+        <AppContainer direction="row">
+            <AlertContainer style={{ position: 'fixed', top: 10, left: 10, right: 10, zIndex: 2 }} />
+            <Diagram />
+            <Sidebar />
+        </AppContainer>
+    )
+}
+
+ReactDOM.render(
+    <DiagramProvider>
+        <App />
+    </DiagramProvider>,
+    document.getElementById('app')
+)
