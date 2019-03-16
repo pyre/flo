2422f042541b020fc4879ef33dd4823ee788073a
Alec Aivazis <alec@aivazis.com>
2019-03-15 23:07:55 -0700

    dried product circle in selection

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,25 +1,44 @@
 // external imports
 import React from 'react'
+import { css } from 'glamor'
 // local imports
 import { productColor, productFillEmpty } from '~/design'
 import { Arc } from '~/components'
 
 export const Radius = 12
 
-const Product = ({ progress, x, y }) => (
-    <svg>
-        // a full circle to designate the zero-progress state
-        <circle fill={productFillEmpty} cx={x} cy={y} r={Radius} />
-        // the primary fill of the product should designate progress
-        <Arc
-            fill={productColor}
-            x={x}
-            y={y}
-            r={Radius}
-            theta1={0}
-            theta2={360 * progress}
-        />
-    </svg>
-)
+const Product = ({ progress, x, y, style, ...unused }) => {
+
+    // if we weren't given an x or y
+    if (!x || !y) {
+        x = Radius
+        y = Radius
+    }
+
+    return (
+        <svg
+            {...css({
+                width: 2 * Radius,
+                height: 2 * Radius,
+                overflow: 'visible',
+                ...style,
+            })}
+            x={x - Radius}
+            y={y - Radius}
+        >
+            // a full circle to designate the zero-progress state
+            <circle fill={productFillEmpty} cx={Radius} cy={Radius} r={Radius} />
+            // the primary fill of the product should designate progress
+            <Arc
+                fill={productColor}
+                x={Radius}
+                y={Radius}
+                r={Radius}
+                theta1={0}
+                theta2={360 * progress}
+            />
+        </svg>
+    )
+}
 
 export default Product
--- client/src/interface/index.js
+++ client/src/interface/index.js
@@ -1,4 +1,3 @@
-export App from './App'
 export Diagram from './Diagram'
 export Flo from './Flo'
 export Sidebar from './Sidebar'
