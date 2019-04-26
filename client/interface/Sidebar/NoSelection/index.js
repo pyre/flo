b417ac4f61acd24bbc5d2041a736e9faf26efab2
Alec Aivazis <alec@aivazis.com>
2019-04-26 15:38:13 -0700

    added tab bar when there's no selection

--- client/interface/Sidebar/NoSelection/index.js
+++ client/interface/Sidebar/NoSelection/index.js
@@ -1,46 +1,63 @@
 // external imports
 import React, { useState } from 'react'
 import { css } from 'glamor'
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs'
 // local imports
-import * as styles from './styles'
 import Header from '../Header'
 import { darkGrey } from '~/design'
 
-export default () => (
-    <div
-        {...css({
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 1,
-        })}
-    >
-        <Header
-            icon={
-                <div
-                    {...css({
-                        background: darkGrey,
-                        borderRadius: 3,
-                        height: 26,
-                        width: 26,
-                    })}
-                />
-            }
-            title={<span {...css({ color: darkGrey })}>Nothing selected</span>}
-        />
-        <div
+export default () => {
+    // the current tab
+    const [tabIndex, setTabIndex] = useState(0)
+
+    return (
+        <Tabs
             {...css({
-                width: 200,
-                alignSelf: 'center',
-                textAlign: 'center',
-                lineHeight: 1.5,
                 display: 'flex',
+                flexDirection: 'column',
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                color: darkGrey,
             })}
+            index={tabIndex}
+            onChange={setTabIndex}
         >
-            Choose a factory or product for more info.
-        </div>
-    </div>
-)
+            <TabList
+                {...css({
+                    flexDirection: 'row',
+                    display: 'flex',
+                    borderBottom: `1px solid #dedede`,
+                    marginBottom: 9,
+                })}
+            >
+                <Tab as="div" {...css(styles.tab)} {...tabIndex !== 0 && css(styles.tabInactive)}>
+                    Products
+                </Tab>
+                <Tab as="div" {...css(styles.tab)} {...tabIndex !== 1 && css(styles.tabInactive)}>
+                    Factories
+                </Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel {...css({ outline: 'none' })}>products</TabPanel>
+                <TabPanel {...css({ outline: 'none' })}>factories</TabPanel>
+            </TabPanels>
+        </Tabs>
+    )
+}
+
+const styles = {
+    tab: {
+        display: 'flex',
+        flexGrow: 1,
+        width: 10,
+        marginTop: -10,
+        height: 42,
+        fontSize: 16,
+        background: 'none',
+        border: 'none',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+    },
+    tabInactive: {
+        color: '#807A73',
+    },
+}
