078464236eaf8399215cdd49741558da50b803ec
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-05-01 08:12:47 -0700

    examples: renamed {names} to {load}, since all it does is load a flow from a configuration file and display it

---
+++ examples/load.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- Python -*-
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# parasim
+# (c) 1998-2019 all rights reserved
+#
+
+
+# get the package
+import flo
+
+# my app
+class Load(flo.flow.workflow, family="flo.applications.load"):
+    """
+    Examine the flow node naming strategy
+    """
+
+
+    # interface
+    @flo.export
+    def main(self, *args, **kwds):
+        """
+        The main entry point
+        """
+        # show me
+        self.pyre_dump(channel=self.info)
+        # all done
+        return 0
+
+
+# bootstrap
+if __name__ == "__main__":
+    # instantiate
+    app = Load(name="load")
+    # invoke
+    status = app.run()
+    # and share
+    raise SystemExit(status)
+
+
+# end of file
