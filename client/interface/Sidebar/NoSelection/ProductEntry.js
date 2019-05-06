0b5f46464a54756bac0d7b5167eaec92e2280032
Alec Aivazis <alec@aivazis.com>
2019-05-06 11:19:30 -0700

    added better content for the product entry in sidebar

--- client/interface/Sidebar/NoSelection/ProductEntry.js
+++ client/interface/Sidebar/NoSelection/ProductEntry.js
@@ -1,14 +1,22 @@
 // external imports
 import React from 'react'
 import { graphql, createFragmentContainer } from 'react-relay'
+import { css } from 'glamor'
+// local imports
+import { Product } from '~/components'
 
-const ProductEntry = ({ product }) => <div>{product.id}</div>
+const ProductEntry = ({ product }) => (
+    <div {...css({ display: 'flex', flexDirection: 'row', marginBottom: 10, alignItems: 'center' })}>
+        <Product progress={1} {...css({ marginRight: 10 })} />
+        <div {...css({})}>{product.name}</div>
+    </div>
+)
 
 export default createFragmentContainer(
     ProductEntry,
     graphql`
         fragment ProductEntry_product on Product {
-            id
+            name
         }
     `
 )
