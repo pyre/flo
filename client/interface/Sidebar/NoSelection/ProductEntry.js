53745947e62f5fa4ec992af9e15da4e00d1b8943
Alec Aivazis <alec@aivazis.com>
2019-05-07 13:21:02 -0700

    product shadow snaps to grid

--- client/interface/Sidebar/NoSelection/ProductEntry.js
+++ client/interface/Sidebar/NoSelection/ProductEntry.js
@@ -1,23 +1,63 @@
 // external imports
-import React, { useRef } from 'react'
+import React, { useRef, useContext } from 'react'
 import { graphql, createFragmentContainer } from 'react-relay'
 import { css, hover } from 'glamor'
 // local imports
 import { Product } from '~/components'
-import { useMouseDrag } from '~/hooks'
+import { useMouseDrag, useRelativePosition } from '~/hooks'
 import { lightGrey, darkGrey } from '~/design'
+import { Diagram } from '~/context'
+import { round } from '~/utils'
 
 const ProductEntry = ({ product }) => {
-    // a ref to track mouse drag
-    const ref = useRef()
+    // a ref to the root of the list item
+    const rootElement = useRef()
 
-    // if we are dragging the mouse
-    const drag = useMouseDrag(ref)
+    // get the diagram info
+    const { diagram } = useContext(Diagram)
+
+    // we need to track mouse drags on the element
+    const drag = useMouseDrag(rootElement)
+    // the location to show the shadow
+    let shadowLocation = null
+
+    // we need to compute the relative position but if we're not dragging just pass null
+    // NOTE: this is here and not in the `if (drag) { ... }` because of the Rule of Hooks
+    const relativePosition = useRelativePosition(drag ? drag.currentLocation : null)
+
+    // we only want to show the shadow if we are dragging
+    if (drag) {
+        // the shadow should follow the mouse as closely as possible
+        shadowLocation = drag.currentLocation
+
+        // if we are dragging the shadow outside of the sidebar
+        if (drag.currentLocation.x < 954) {
+            // grab the diagram info we need
+            const { gridSize, pan } = diagram
+
+            // the x coordinate of the first vertical grid line visible
+            const gridLeftX = pan.x < 0 ? gridSize + (pan.x % gridSize) : pan.x % gridSize
+            // compute the difference between the left grid and the mouse
+            const nGridsWide = round(drag.currentLocation.x - gridLeftX, gridSize)
+
+            // the y coordinate of the first horzintaol grid line visible
+            const gridTopY = pan.y < 0 ? gridSize + (pan.y % gridSize) : pan.y % gridSize
+            // compute the difference between the left grid and the mouse
+            const nGridsTall = round(drag.currentLocation.y - gridTopY, gridSize)
+
+            // the y coordinate of the first horizontal grid line visible
+            // we should snap the shadow to the grid
+            shadowLocation = {
+                x: gridLeftX + nGridsWide,
+                y: gridTopY + nGridsTall,
+            }
+        }
+    }
 
     return (
         <>
             <div
-                ref={ref}
+                ref={rootElement}
                 {...css({
                     display: 'flex',
                     flexDirection: 'row',
@@ -28,6 +68,7 @@ const ProductEntry = ({ product }) => {
                     alignItems: 'center',
                     cursor: 'move',
                     userSelect: 'none',
+                    backgroundColor: drag ? lightGrey : 'none',
                 })}
                 {...hover({
                     backgroundColor: lightGrey,
@@ -39,13 +80,14 @@ const ProductEntry = ({ product }) => {
                     <div {...css({ color: darkGrey })}>{product.description}</div>
                 </div>
             </div>
-            {drag && (
+            {shadowLocation && (
                 <Product
                     progress={1}
                     style={{
                         position: 'fixed',
-                        top: drag.currentLocation.y,
-                        left: drag.currentLocation.x,
+                        top: shadowLocation.y,
+                        left: shadowLocation.x,
+                        transform: 'translateX(-50%) translateY(-50%)',
                     }}
                 />
             )}
