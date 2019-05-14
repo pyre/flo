4d1c860932b95cad97f048ce054cbdf0053fdbeb
Alec Aivazis <alec@aivazis.com>
2019-05-14 10:42:54 -0700

    product tooltips show correct progress

--- client/interface/Flo/Product.js
+++ client/interface/Flo/Product.js
@@ -13,54 +13,6 @@ import { Radius } from '~/components/Product'
 // the radius of the inner circle
 const gutter = 15
 
-const Tooltip = ({ product, ...props }) => {
-    // we need to know the current translation on the diagram to follow it
-    const { diagram } = useContext(Diagram)
-
-    // we need to render the portal outside of the immediate dom tree so we can render HTML
-    // without the annoyance of embedding the element in a foreignObject (and have to have a definite width)
-    return (
-        <Portal id={`tooltip-${product.id}`}>
-            <div
-                {...css({
-                    position: 'fixed',
-                    zIndex: 10,
-                    transform: 'translate(-50%, -50%)',
-                    height: 50,
-                    backgroundColor: 'white',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    padding: 12,
-                    borderRadius: 3,
-                    boxShadow: '0 3px 6px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)',
-                })}
-                style={{
-                    top: product.position.y - 50 + diagram.pan.y,
-                    left: product.position.x + diagram.pan.x,
-                }}
-            >
-                <ProductCircle
-                    progress={1}
-                    {...css({
-                        marginRight: 8,
-                    })}
-                />
-                <div
-                    {...css({
-                        display: 'flex',
-                        flexDirection: 'column',
-                    })}
-                >
-                    <div>{product.name}</div>
-                    <div>{product.description}</div>
-                </div>
-            </div>
-        </Portal>
-    )
-}
-
 const Product = ({ product }) => {
     // grab the diagram selected state
     const { diagram } = useContext(Diagram)
@@ -180,6 +132,54 @@ const Product = ({ product }) => {
             </Draggable>
             {diagram.showTooltips && <Tooltip product={product} x={product.position.x} y={product.position.y - 50} />}
         </>
+    )
+}
+
+const Tooltip = ({ product, ...props }) => {
+    // we need to know the current translation on the diagram to follow it
+    const { diagram } = useContext(Diagram)
+
+    // we need to render the portal outside of the immediate dom tree so we can render HTML
+    // without the annoyance of embedding the element in a foreignObject (and have to have a definite width)
+    return (
+        <Portal id={`tooltip-${product.id}`}>
+            <div
+                {...css({
+                    position: 'fixed',
+                    zIndex: 10,
+                    transform: 'translate(-50%, -50%)',
+                    height: 50,
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    padding: 12,
+                    borderRadius: 3,
+                    boxShadow: '0 3px 6px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23)',
+                })}
+                style={{
+                    top: product.position.y - 50 + diagram.pan.y,
+                    left: product.position.x + diagram.pan.x,
+                }}
+            >
+                <ProductCircle
+                    progress={product.progress}
+                    {...css({
+                        marginRight: 8,
+                    })}
+                />
+                <div
+                    {...css({
+                        display: 'flex',
+                        flexDirection: 'column',
+                    })}
+                >
+                    <div>{product.name}</div>
+                    <div>{product.description}</div>
+                </div>
+            </div>
+        </Portal>
     )
 }
 
