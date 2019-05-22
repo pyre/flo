35d661099795ec3397c94a8d25923922011df175
Alec Aivazis <alec@aivazis.com>
2019-05-22 12:00:38 -0700

    fixed and removed unused imports

--- client/interface/Sidebar/NoSelection/FactorySidebarEntry.js
+++ client/interface/Sidebar/NoSelection/FactorySidebarEntry.js
@@ -1,8 +1,6 @@
 // external imports
 import React from 'react'
 import { graphql, createFragmentContainer } from 'react-relay'
-import pluralize from 'pluralize'
-import { css } from 'glamor'
 // local imports
 import { Factory, FactoryIOString } from '~/components'
 import SidebarEntry from './SidebarEntry'
