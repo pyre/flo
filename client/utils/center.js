fd33981fb0439de30803bd6c31f86fdf3554c738
Alec Aivazis <alec@aivazis.com>
2019-05-31 14:36:11 -0700

    center correctly positions in normal form

--- client/utils/center.js
+++ client/utils/center.js
@@ -1,21 +1,31 @@
-const gridSize = 50
+// this should be twice the grid size
+const spacing = 100
 
 export default function center(origin, nItems) {
     // the final list of points
     const points = []
 
     const nTop = Math.floor(nItems / 2)
+    const isEven = nItems % 2 == 0
 
     // add the points above the origin
     for (let i = nTop; i > 0; i--) {
+        // if there is an even number of points, then we have to bring
+        // this down half a spacing to compensate for the lack of middle point
+        let y = origin.y - i * spacing
+        if (isEven) {
+            y += spacing / 2
+        }
+
+        // add the point to the list
         points.push({
             x: origin.x,
-            y: origin.y - i * gridSize,
+            y,
         })
     }
 
     // if there is an odd number
-    if (nItems % 2) {
+    if (!isEven) {
         // we have to add a point in the middle
         points.push({
             x: origin.x,
@@ -25,9 +35,17 @@ export default function center(origin, nItems) {
 
     // add the points below the origin
     for (let i = 1; i <= nTop; i++) {
+        // if there is an even number of points, then we have to bring
+        // this up half a spacing to compensate for the lack of middle point
+        let y = origin.y + i * spacing
+        if (isEven) {
+            y -= spacing / 2
+        }
+
+        // add the point to the list
         points.push({
             x: origin.x,
-            y: origin.y + i * gridSize,
+            y,
         })
     }
 
--- client/utils/center.test.js
+++ client/utils/center.test.js
@@ -5,5 +5,5 @@ test('centers even ranges', () => {
 })
 
 test('centers odd range', () => {
-    expect(center({ x: 0, y: 0 }, 3)).toEqual([{ x: 0, y: -50 }, { x: 0, y: 0 }, { x: 0, y: 50 }])
+    expect(center({ x: 0, y: 0 }, 3)).toEqual([{ x: 0, y: -100 }, { x: 0, y: 0 }, { x: 0, y: 100 }])
 })
